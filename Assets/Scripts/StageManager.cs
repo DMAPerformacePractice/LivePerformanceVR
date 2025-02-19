ff586180c16afddec2523be55cb10fd4411dc911
Asvaka <78432238+Asvaka@users.noreply.github.com>
2025-02-18 22:45:00 -0800

    Got Random Audience Interruptions Working
    Timing is currently a little off, needs some debugging, and animations aren't in yet.
    General system seems to work though.

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,13 +10,31 @@ public class StageManager : MonoBehaviour
     private bool lightsDimming = false;
 
     public static bool userPerforming = false;
+    public static event Action<StageManager> OnPerformaceStartEvent;
 
     [SerializeField] private float dimTime = 2;
+
+    [SerializeField] static private AudienceInterruption[] audienceInterruptions;
+
+    private void Awake()
+    {
+        var interruptions = Resources.LoadAll("Interruptions");
+
+        audienceInterruptions = new AudienceInterruption[interruptions.Length];
+
+        for (int i = 0; i < interruptions.Length; i++)
+        {
+            if (interruptions[i] is AudienceInterruption)
+            {
+                audienceInterruptions[i] = (AudienceInterruption) interruptions[i];
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,15 +42,19 @@ public class StageManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            StartLightsDimming();
+            StartPerformance();
         }
     }
 
-    public void StartLightsDimming()
+    public void StartPerformance()
     {
-        if (lightsDimming == false)
+        if (userPerforming == false)
         {
-            StartCoroutine(DimLights());
+            OnPerformaceStartEvent(this);
+            if (lightsDimming == false)
+            {
+                StartCoroutine(DimLights());
+            }
         }
     }
 
@@ -50,5 +73,10 @@ public class StageManager : MonoBehaviour
         }
 
         lightsDimming = false;
+    }
+
+    public static AudienceInterruption[] GetAudienceInterruptions()
+    {
+        return audienceInterruptions;
     }
 }
