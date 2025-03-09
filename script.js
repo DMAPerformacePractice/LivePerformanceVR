808bfbb6210aa321666049a1bffa39ca8184d823
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2025-03-09 15:36:42 -0700

    Created script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("startBtn").addEventListener("click", function() {
+    window.location.href = "performance.html";
+});
+
+document.getElementById("optionsBtn").addEventListener("click", function() {
+    window.location.href = "options.html";
+});
+
+document.getElementById("exitBtn").addEventListener("click", function() {
+    window.location.href = "exit.html";
+});
