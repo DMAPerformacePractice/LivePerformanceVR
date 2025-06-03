410dae5db5f42775f251b8d641137d3de4d45ccf
Asvaka <78432238+Asvaka@users.noreply.github.com>
2025-06-03 12:55:07 -0700

    Clean Up EndPerformanceMotionDetector.cs

--- Assets/Scripts/EndPerformanceMotionDetector.cs
+++ Assets/Scripts/EndPerformanceMotionDetector.cs
@@ -6,20 +6,6 @@ public class EndPerformanceMotionDetector : MonoBehaviour
 {
     [SerializeField] private StageManager stageManager;
 
-    private BoxCollider collider;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        collider = GetComponent<BoxCollider>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         stageManager.EndPerformance();
